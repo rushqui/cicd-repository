23a91fbbeb09388ca9b1804d5c1f81c2f1af781e
Elias Rojas <crojasa@aeromexico.com>
2025-01-08 21:33:54 -0600

    updating amazon linux standard image v5 an node version 22 in buildspec

--- 2frontend-pipeline.tf
+++ 2frontend-pipeline.tf
@@ -101,7 +101,7 @@ resource "aws_codebuild_project" "tf-frontend1" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:4.0"
+    image                       = "aws/codebuild/standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
